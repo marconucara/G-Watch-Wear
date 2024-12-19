a406f3b860c9410cb6ba1184187559ec6d36bb1b
Marco Nucara <mn@empatica.com>
2024-12-19 14:35:09 +0000

    fix: handle accountId header in LibreLinkUpFollower service - cleanup

--- mobile/src/main/java/sk/trupici/gwatch/wear/followers/LibreLinkUpFollowerService.java
+++ mobile/src/main/java/sk/trupici/gwatch/wear/followers/LibreLinkUpFollowerService.java
@@ -35,7 +35,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HexFormat;
 import java.util.List;
 import java.util.Locale;
 
